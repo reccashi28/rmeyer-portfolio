0f213503fa085ea32b2dfbe43030bc7d34b468a5
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-07-06 20:51:01 +0200

    update overall look

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const menu = document.querySelector('.navbar');
 const hamburger = document.querySelector('.hamburger-menu');
 const navItem = document.querySelectorAll('.nav-item');
-const projectName = document.querySelector('.proj-name');
-const projectBtn = document.querySelector('.proj-btn');
+const projectName = document.querySelector('.project__name');
+const projectBtn = document.querySelector('.project__btn');
 const gotoTop = document.querySelector('.fa-arrow-up');
 const resumeBtn = document.querySelector('.to-project-btn');
+const projectContentBox2 = document.querySelector('.project__content2');
+
+let projects = [];
 
 //eventlistener
 hamburger.addEventListener('click', () => {
@@ -41,7 +44,38 @@ gotoTop.addEventListener('click', () => {
   });
 });
 
+//fetch project content
+const getProjects = async () => {
+  try{
+    const res = await fetch('data.json')
+    projects = await res.json()
+    console.log(projects, "projects")
+    loadProjects2(projects)
+  }catch (e) {
+    alert(`Error occured ${e}`)
+  }
+}
+getProjects()
 
+const loadProjects2 = (data) => {
+  let projectList2 = data.map( project => {
+    return `
+      <div class="project__list col1 col is-visible">
+        <h2>${project.title}</h2>
+        <div class="project__list__content">
+          <img src="${project.imgSource}" alt="${project.title}">
+          <p>${project.description}</p>
+        </div>
+        <div class="project__col__button">
+          <span><a href="${project.website}" target="_blank">Visit Site</a></span>
+          <span><a href="${project.sourceCode}" target="_blank">Code</a></span>
+        </div>
+        
+      </div>
+    `
+  }).join('');
+  projectContentBox2.insertAdjacentHTML('beforeend', projectList2);
+}
 //animations
 const scroll = window.requestAnimationFrame || function( callback ) { 
     window.setTimeout( callback, 1000/60)};
